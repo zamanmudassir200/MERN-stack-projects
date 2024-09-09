caf3004e2c603c4fa9b27e3bdec7b37493df4767
Muhammad Mudassir Zaman <127381696+zamanmudassir200@users.noreply.github.com>
2024-09-09 10:35:17 +0500

    hiding 2 field on mobile devices

--- BookStore/frontend/src/components/home/BookTable.jsx
+++ BookStore/frontend/src/components/home/BookTable.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { FaEdit } from "react-icons/fa";
 import { BsInfoCircle } from "react-icons/bs";
-import { MdOutlineAddBox, MdOutlineDelete } from "react-icons/md";
+import { FaEdit } from "react-icons/fa";
+import { MdOutlineDelete } from "react-icons/md";
 const BookTable = ({ books }) => {
   return (
-    <table className="w-full border-spacing-2">
+    <table className="w-full border-spacing-2 overflow-x-scroll">
       <thead>
         <tr>
           <th className="border border-slate-400 rounded-md">No</th>
@@ -22,17 +22,17 @@ const BookTable = ({ books }) => {
       <tbody>
         {books.map((book, index) => {
           return (
-            <tr key={book._id} className="h-10">
-              <td className="border border-slate-700 rounded-md text-center ">
+            <tr key={book._id} className="h-10 ">
+              <td className="border p-1 border-slate-700 rounded-md text-center ">
                 {index + 1}
               </td>
               <td className="border border-slate-700 rounded-md text-center ">
                 {book.title}
               </td>
-              <td className="border border-slate-700 rounded-md text-center ">
+              <td className="border border-slate-700 rounded-md text-center max-md:hidden ">
                 {book.author}
               </td>
-              <td className="border border-slate-700 rounded-md text-center ">
+              <td className="border border-slate-700 rounded-md text-center max-md:hidden">
                 {book.publishYear}
               </td>
               <td className="border border-slate-700 rounded-md text-center ">
