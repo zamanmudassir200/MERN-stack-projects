57ce513c919ad690a9198c3a4092a7c3d31b2675
Muhammad Mudassir Zaman <127381696+zamanmudassir200@users.noreply.github.com>
2024-09-09 10:14:11 +0500

    wrap whole app with SnackbarProvider

--- BookStore/frontend/src/main.jsx
+++ BookStore/frontend/src/main.jsx
@@ -3,9 +3,11 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-
+import { SnackbarProvider } from "notistack";
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <App />
+    <SnackbarProvider>
+      <App />
+    </SnackbarProvider>
   </BrowserRouter>
 );
