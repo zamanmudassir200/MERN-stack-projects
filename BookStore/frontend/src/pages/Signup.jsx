af42549f8ea042072f914fca938f5503720d4fd9
Muhammad Mudassir Zaman <127381696+zamanmudassir200@users.noreply.github.com>
2024-09-15 10:54:40 +0500

    url added into frontend

--- BookStore/frontend/src/pages/Signup.jsx
+++ BookStore/frontend/src/pages/Signup.jsx
@@ -25,7 +25,7 @@ const Signup = () => {
 
     try {
       const response = await fetch(
-        "https://mern-stack-projects-ten.vercel.app/auth/signup",
+        "https://mern-stack-projects-ten.vercel.app/signup",
         {
           method: "POST",
           headers: {
