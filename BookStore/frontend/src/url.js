0d26abcc5a36569cc7c21e701160de6b381d4cfc
zamanmudassir200 <zamanmudassir200@gmail.com>
2025-03-08 13:07:33 +0500

    url changed to deployed url

--- BookStore/frontend/src/url.js
+++ BookStore/frontend/src/url.js
@@ -1,3 +1,3 @@
-// const url = "https://mern-stack-projects-iota.vercel.app";
-const url = "http://localhost:4000";
+const url = "https://mern-stack-projects-iota.vercel.app";
+// const url = "http://localhost:4000";
 export default url;
